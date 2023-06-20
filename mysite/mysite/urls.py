d167bb86bff163db5d54918a284196d61e55f49b
Amitabh Suman <amitabhsuman.ss89@gmail.com>
2023-06-20 07:26:10 +0530

    Added home page

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -18,11 +18,13 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("myapp/", include('myapp.urls')),
-    path("accounts/", include('django.contrib.auth.urls'))
+    path("accounts/", include('django.contrib.auth.urls')),
+    path("", RedirectView.as_view(pattern_name='myapp:product_list'), name="home")
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
